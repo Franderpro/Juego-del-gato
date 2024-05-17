f06f705915c148539197eb4ba2c670fd5c1e7fee
Franderprogra <franderprogra@gmail.com>
2024-05-17 15:08:06 -0600

    css login y registrolisto

---
+++ java/juego.js
@@ -0,0 +1,72 @@
+let valor = document.getElementsByClassName("gato")
+
+
+    
+
+
+    
+
+valor[0].addEventListener("click",function () {     
+    let gif = document.createElement("img")
+    gif.src = "./img/giphy.gif";
+    gif.style.height="80px";
+    valor[0].appendChild(gif);
+})
+
+valor[1].addEventListener("click",function () {
+    let gif = document.createElement("img")
+    gif.src = "./img/giphy.gif";
+    gif.style.height="80px";
+    valor[1].appendChild(gif);
+})
+valor[2].addEventListener("click",function () {
+    let gif = document.createElement("img")
+    gif.src = "./img/giphy.gif";
+    gif.style.height="80px";
+    valor[2].appendChild(gif) ;
+})
+valor[3].addEventListener("click",function () {
+    let gif = document.createElement("img")
+    gif.src = "./img/giphy.gif";
+    gif.style.height="80px";
+    valor[3].appendChild(gif);
+})
+valor[4].addEventListener("click",function () {
+    let gif = document.createElement("img")
+    gif.src = "./img/giphy.gif";
+    gif.style.height="80px";
+    valor[4].appendChild(gif);
+})
+valor[5].addEventListener("click",function () {
+    let gif = document.createElement("img")
+    gif.src = "./img/giphy.gif";
+    gif.style.height="80px";
+    valor[5].appendChild(gif);
+})
+valor[6].addEventListener("click",function () {
+    let gif = document.createElement("img")
+    gif.src = "./img/giphy.gif";
+    gif.style.height="80px";
+    valor[6].appendChild(gif);
+})
+valor[7].addEventListener("click",function () {
+    let gif = document.createElement("img")
+    gif.src = "./img/giphy.gif";
+    gif.style.height="80px";
+    valor[7].appendChild(gif);
+})
+valor[8].addEventListener("click",function () {
+    let gif = document.createElement("img")
+    gif.src = "./img/giphy.gif";
+    gif.style.height="80px";
+    valor[8].appendChild(gif);  
+})
+valor[9].addEventListener("click",function () {
+    let gif = document.createElement("img")
+    gif.src = "./img/giphy.gif";
+    gif.style.height="80px";
+    valor[9].appendChild(gif);  
+})
+
+
+
