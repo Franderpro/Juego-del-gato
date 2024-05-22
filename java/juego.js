2ebbaf0dbae20fe2df2e87ad8025ef2ccd522ffa
Franderpro <rfrander11@gmail.com>
2024-05-22 13:35:22 -0600

    terminando funcionalidades

--- java/juego.js
+++ java/juego.js
@@ -2,6 +2,9 @@ let i=0
 let usuario = "jerry"
 let jerry = "./img/jerry.gif"
 let tom = "./img/tom.gif"
+
+let ganadas =0;
+
 let jugador = document.getElementById("jugador");
 jugador.innerHTML= "juega jerry"
 
@@ -35,44 +38,70 @@ function ganar(personaje) {
     
     //revisa primera fila  
     if (valor[0].classList[1] === personaje && valor[1].classList[1] === personaje && valor[2].classList[1] === personaje) {      
-        limpiar()
+        setTimeout(() => {
+            limpiar()
+        }, 3000);
+       
         return true;
         
 
     }
     // revisa segunda fila
     else if (valor[3].classList[1] === personaje && valor[4].classList[1] === personaje && valor[5].classList[1] === personaje) {       
-        limpiar()
+        setTimeout(() => {
+            limpiar()
+        }, 3000);
+       
         return true;
     }
     //revisa tercera fila
     else if (valor[6].classList[1] === personaje && valor[7].classList[1] === personaje && valor[8].classList[1] === personaje) {      
-        limpiar()
+        setTimeout(() => {
+            limpiar()
+        }, 3000);
+       
         return true;
     }
     //revisa primera columna
     else if (valor[0].classList[1] === personaje && valor[3].classList[1] === personaje && valor[6].classList[1] === personaje) {        
-        limpiar()
+        setTimeout(() => {
+            limpiar()
+        }, 3000);
+       
         return true;
     }
     //revisa segunda columna
     else if (valor[1].classList[1] === personaje && valor[4].classList[1] === personaje && valor[7].classList[1] === personaje) {       
-        limpiar()
+        setTimeout(() => {
+            limpiar()
+        }, 3000);
+       
         return true;
     }
     // revisa tercera columna
     else if (valor[2].classList[1] === personaje && valor[5].classList[1] === personaje && valor[8].classList[1] === personaje) { 
-        limpiar()
+        setTimeout(() => {
+            limpiar()
+        }, 3000);
+       
         return true;
     }
     // revisa primera diagonal
     else if (valor[0].classList[1] === personaje && valor[4].classList[1] === personaje && valor[8].classList[1] === personaje) {
-        limpiar()
+        setTimeout(() => {
+            limpiar()
+        }, 3000);
+       
         return true;
     }
     //revisa segunda linea
     else if (valor[2].classList[1] === personaje && valor[4].classList[1] === personaje && valor[6].classList[1] === personaje) {
-        limpiar()
+       
+        setTimeout(() => {
+            limpiar()
+        }, 3000);
+       
+      
         return true;
     }
 return false;
@@ -82,6 +111,7 @@ let valor = document.getElementsByClassName("gato")
 for (let index = 0; index < valor.length; index++) { 
     valor[index].addEventListener("click", function () {
         let gif;
+        let jugar=true
         if (valor[index].childElementCount == 0) {
             gif = document.createElement("img")
             if (usuario == "jerry") {
@@ -98,23 +128,37 @@ for (let index = 0; index < valor.length; index++) {
 
         console.log("JERRY ADENTRO")
        
-        valor[index].appendChild(gif);
+        
+
+       
+            valor[index].appendChild(gif);  
+      
          
         setTimeout(() => {
             jugador.innerHTML= "juega jerry" 
         }, 1000);
 
         
-        console.log("indext",index);
-        if (ganar("jerry")) {
-            alert(" jerry gano perdedor")  
+        
+        if (ganar("jerry")) { 
+            
+            ganadas++
+            partidas.innerHTML= ganadas;
+                alert(" jerry gano perdedor") 
+                
+
+            return;
+
         }
         if (i<9) {
+            console.log("hola");
             juegaCompu()
             if (ganar("tom")) {
+                ganadas++
+            partidas2.innerHTML= ganadas;
                 alert("tom gano perdedor")
             }
-           
+            
         }  
     });
 }
@@ -122,6 +166,9 @@ for (let index = 0; index < valor.length; index++) {
 function limpiar() {
     for (let index = 0; index < valor.length; index++) {
         valor[index].innerHTML = "";
+        valor[index].classList.remove("tom")
+        valor[index].classList.remove("jerry")
         
     }
+    i=0
 }
